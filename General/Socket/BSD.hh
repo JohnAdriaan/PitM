641946fb1aa501b2a878165cef194cece9ad97a9
John Adriaan <John.Adriaan@BigPond.com>
2017-11-18 11:05:25 +1100

    Rearranged Socket

---
+++ General/Socket/BSD.hh
@@ -0,0 +1,27 @@
+//
+// BSD.hh
+//
+
+#ifndef BSD_hh
+#define BSD_hh
+
+#include "../General.hh"
+
+namespace BSD {
+
+class _BSD_;
+
+class Socket;
+
+class Listen;
+
+class TCP;
+
+class Raw;
+
+} // BSD
+
+class BSD::_BSD_ : private _General_ {
+}; // _BSD_
+
+#endif // BSD_hh
