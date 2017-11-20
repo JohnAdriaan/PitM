d9801e87c87f18f7ae5f0d6ab2aa28c1b83fbe6d
JohnAdriaan <John.Adriaan@BigPond.com>
2017-11-20 16:45:25 +1100

    Get started on Worker processing. I can see HTTP and HTML classes on my horizon.

--- Server/Worker.cc
+++ Server/Worker.cc
@@ -2,7 +2,11 @@
 // Worker.cc
 //
 
+#include <iostream>
+
 #include "Worker.hh"
+
+#include "../PitM.hh"
 
 Worker::Worker(BSD::TCP &client, const BSD::Address &address) :
         TCP(client),
@@ -18,6 +22,18 @@ Worker::Worker(BSD::TCP &client, const BSD::Address &address) :
 } // Worker::Worker(client, address)
 
 void *Worker::Run() {
+   unsigned read;
+   char buffer[1024+1];
+   while (Read(buffer, sizeof buffer-1, read)) {
+      buffer[read] = '\0';
+      std::cout << buffer;
+      if (buffer[0]=='q' && buffer[1]=='u' && buffer[2]=='i' && buffer[3]=='t') {
+         quit.Post();
+      } // if
+      if (read==0) {
+         Close();
+      } // if
+   } // while
    delete this;
    return nullptr;
 } // Worker::Run()
