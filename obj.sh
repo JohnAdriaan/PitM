10ffd8a87f295bc45ba2a4aa61be16865f231dd4
JohnAdriaan <John.Adriaan@BigPond.com>
2017-11-24 10:41:05 +1100

    Generalise obj script

--- obj.sh
+++ obj.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+arch=arm
+obj=ef32-littlearm
 file="${1##*/}"
 ext="${file##*.}"
 file="${file%.*}"
-objcopy -I binary -B arm -O elf32-littlearm                        \
-        --redefine-sym _binary____"$file"_"$ext"_start="$file"    \
-        --redefine-sym _binary____"$file"_"$ext"_end="$file"End    \
+objcopy -I binary -B $arch -O $obj                              \
+        --redefine-sym _binary____"$file"_"$ext"_start="$file"   \
+        --redefine-sym _binary____"$file"_"$ext"_end="$file"End   \
         --redefine-sym _binary____"$file"_"$ext"_size="$file"Size  \
         "$1" "$file".o
