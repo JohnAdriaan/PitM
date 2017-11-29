d12e43ff42437758e96100fe4f2bc2dd411286a8
JohnAdriaan <John.Adriaan@BigPond.com>
2017-11-29 12:59:52 +1100

    Added named Semaphores

--- General/Thread/Semaphore.cc
+++ General/Thread/Semaphore.cc
@@ -2,26 +2,100 @@
 // Semaphore.cc
 //
 
+#include <fcntl.h>    // For O_*
+#include <sys/stat.h> // For S_*
+
 #include "Semaphore.hh"
+
+//static const int share = S_IRUSR|S_IWUSR | S_IRGRP|S_IWGRP | S_IROTH|S_IWOTH;
+static const int share = S_IREAD|S_IWRITE;
 
 using namespace MT;
 
-Semaphore::Semaphore(unsigned start/*=0*/) {
-   sem_init(&semaphore, 0, start);
+Semaphore::Semaphore(unsigned start/*=0*/) :
+           name(nullptr),
+           sem(),
+           semaphore(&sem) {
+   ::sem_init(semaphore, 0, start);
 } // Semaphore::Semaphore(start)
 
+Semaphore::Semaphore(const char *name,
+                     Creates create/*=Exists*/,
+                     unsigned start/*=0*/) :
+           name(name),
+           sem(),
+           semaphore(SEM_FAILED) {
+   int oflags;
+   switch (create) {
+   case Exists :
+      oflags = O_RDWR;
+      break;
+   case Create :
+      oflags = O_RDWR|O_CREAT;
+      break;
+   case Exclusive :
+      oflags = O_RDWR|O_CREAT|O_EXCL;
+      break;
+   } // switch
+   semaphore = ::sem_open(name, oflags, share, start);
+} // Semaphore::Semaphore(name,create,start)
+
+Semaphore::Semaphore(const char *name, bool &already) :
+           Semaphore(name, Exclusive) { // Attempt to create
+   already = false;
+   if (Valid()) {
+      return;
+   } // if
+   semaphore = ::sem_open(name, O_RDWR);     // Attempt to open existing
+   if (Valid()) {
+      already = true;
+      return;
+   } // if
+   semaphore = ::sem_open(name, O_RDWR|O_CREAT, share, 0); // Something's odd...
+} // Semaphore::Semaphore(name,already)
+
 bool Semaphore::Post() {
-   return sem_post(&semaphore)==0;
+   if (!Valid()) {
+      return false;
+   } // if
+   return ::sem_post(semaphore)==0;
 } // Semaphore::Post()
 
 bool Semaphore::Try() {
-   return sem_trywait(&semaphore)==0;
+   if (!Valid()) {
+      return false;
+   } // if
+   return ::sem_trywait(semaphore)==0;
 } // Semaphore::Try()
 
 void Semaphore::Wait() {
-   sem_wait(&semaphore);
+   if (!Valid()) {
+      return;
+   } // if
+   ::sem_wait(semaphore);
 } // Semaphore::Wait()
 
+void Semaphore::Unlink() {
+   if (name==nullptr) {
+      return;
+   } // if
+   ::sem_unlink(name);
+} // Semaphore::Unlink()
+
+void Semaphore::Close() {
+   sem_t *temp = semaphore;
+   semaphore = SEM_FAILED;
+   if (temp==SEM_FAILED) {
+      return;
+   } // if
+   if (temp!=&sem) { // Named?
+      ::sem_close(temp);
+   } // if
+   else {
+      ::sem_destroy(temp);
+   } // else
+} // Semaphore::Close()
+
 Semaphore::~Semaphore() {
-   sem_destroy(&semaphore);
+   Close();
 } // Semaphore::~Semaphore()
--- General/Thread/Semaphore.hh
+++ General/Thread/Semaphore.hh
@@ -11,9 +11,23 @@
 
 class MT::Semaphore : private _MT_ {
 
+public: // Enums
+
+   enum Creates {
+      Exists,   // Open only if exists
+      Create,   // Create if doesn't exist
+      Exclusive // Only open if doesn't exist
+   }; // Creates
+
 public: // Methods
 
    explicit Semaphore(unsigned start=0);
+
+   explicit Semaphore(const char *name, Creates create=Exists, unsigned start=0);
+
+   Semaphore(const char *name, bool &already);
+
+   inline bool Valid() const;
 
    bool Post();
 
@@ -21,16 +35,29 @@ public: // Methods
 
    void Wait();
 
+   void Close();
+
+   // Remove named Semaphore from future Opens
+   void Unlink();
+
    ~Semaphore();
 
 private: // Methods
 
-   Semaphore(const Semaphore &) = delete;
+   explicit Semaphore(const Semaphore &) = delete;
 
 private: // Variables
 
-   sem_t semaphore;
+   const char *name;
+
+   sem_t sem;
+
+   sem_t *semaphore;
 
 }; // Semaphore
+
+inline bool MT::Semaphore::Valid() const {
+   return semaphore!=SEM_FAILED;
+} // Semaphore::Valid()
 
 #endif // Semaphore_hh
